a8dba3bd5445f6757515fc7d2eca4bacaad9ab2b
Davis McClure <dmcclure40@gmail.com>
2016-09-07 16:33:46 -0600

    Lab 1 save 2 all content no formatting (unfinished)

--- Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/ViewController.swift
@@ -54,13 +54,13 @@ class ViewController: UIViewController {
     
     @IBAction func selectDetails(sender: UIButton) {
         if sender.currentTitle=="Raid"{
-            details.text="Wrath of the Machine"
+            details.text="The new raid will feature the Devil Splicers a new variant of the fallen enemy faction. This is the first raid featuring the fallen enemies. This raid will begain in a social area called the plaugelands. Unlike previous raids, there is a fair bit of outdoor action in this, and you can see the area from other parts of the Plaguelands. The raid will release on September 23rd"
         }
         else if sender.currentTitle=="Enemy"{
-            details.text="Devil Splicers"
+            details.text="The House of Devels are a group of Fallen scavengers that has come across a nanotechnology called SIVA. This technology was designed to 'Defend and Protect' and has a very aggressive, and militaristic nature to it. The Devils have augmented themselves and their wepons with this nano-tech to beceome more deadly and aggressive. Now that the Devil Splicers have be created this group of Fallen will be faster and more leathal then ever before."
         }
-        else if sender.currentTitle=="Archon Forge"{
-            details.text="Archon's Forge"
+        else if sender.currentTitle=="Arena"{
+            details.text="The Archon's Forge is a new arena mode set inside of the plauglands. Players can enter and be challeneged with waves of enemies in varying difficulty. Each encounter lasts about 5 minutes long and is a combination of Prison of Elders and the Court of Oryx areans. Players can join at any time, however if you die in the arena you are sent into the 'stands' to watch the rest of the battle."
         }
     }   
     
